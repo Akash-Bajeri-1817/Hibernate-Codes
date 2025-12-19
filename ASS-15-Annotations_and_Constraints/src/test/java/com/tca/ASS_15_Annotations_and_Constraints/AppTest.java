fbe2b96d23ae250a39ea0c7bd2a9e7dca477a638
Akash-Bajeri-1817 <akashbajeri18@gmail.com>
2025-12-19 22:03:26 +0530

    added constraints through annotations

---
+++ ASS-15-Annotations_and_Constraints/src/test/java/com/tca/ASS_15_Annotations_and_Constraints/AppTest.java
@@ -0,0 +1,19 @@
+package com.tca.ASS_15_Annotations_and_Constraints;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

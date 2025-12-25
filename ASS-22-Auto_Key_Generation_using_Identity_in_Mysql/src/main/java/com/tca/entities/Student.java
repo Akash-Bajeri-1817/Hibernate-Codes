df0d9a91bbb584a9c62067254d5f79f96d55d822
Akash-Bajeri-1817 <akashbajeri18@gmail.com>
2025-12-25 22:20:54 +0530

    Generated primary key using Identity, Table in PgSql and MySql'

---
+++ ASS-22-Auto_Key_Generation_using_Identity_in_Mysql/src/main/java/com/tca/entities/Student.java
@@ -0,0 +1,55 @@
+package com.tca.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tca_student")
+public class Student 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "roll_number")
+	private Integer rollNumber;
+	
+	@Column(name = "student_name", nullable =false)
+	private String name;
+	
+	@Column(name = "percentage", nullable = false, columnDefinition = "float check(percentage >0 and percentage < 100)")
+	private Double percentage;
+
+	public Integer getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(Integer rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(Double percentage) {
+		this.percentage = percentage;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNumber=" + rollNumber + ", name=" + name + ", percentage=" + percentage + "]";
+	}
+	
+	
+}
---
+++ ASS-23-Auto_Key_Generation_using_Table_in_Mysql/src/main/java/com/tca/App.java
@@ -0,0 +1,46 @@
+package com.tca;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.tca.entities.Student;
+
+public class App
+{
+    public static void main(String[] args) 
+    {
+        Configuration configuration = null;
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction transaction = null;
+        
+        try 
+        {
+        	configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        	sessionFactory = configuration.buildSessionFactory();
+        	session = sessionFactory.openSession();
+        	transaction = session.beginTransaction();
+        	
+        	Student student = new Student();
+        	student.setPercentage(30.0);
+        	student.setName("Bajeri");
+        	
+        	session.persist(student);
+        	Integer rno = (Integer) session.getIdentifier(student);
+        	System.out.println("Record saves for roll number : " + rno);
+        	transaction.commit();
+			
+		} catch (Exception e) 
+        {
+			transaction.rollback();
+			e.printStackTrace();
+		}
+        finally 
+        {
+			session.close();
+			sessionFactory.close();
+		}
+    }
+}
